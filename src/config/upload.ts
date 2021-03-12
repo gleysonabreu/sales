ec4f7c037b821ca90e3cf55e4235c51a71110594
Gleyson Abreu <gleyson_datu@hotmail.com>
2021-03-12 10:01:00 -0300

    Add providers to upload for disk storage and amazon s3

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -1,18 +1,43 @@
 import path from 'path';
-import multer from 'multer';
+import multer, { StorageEngine } from 'multer';
 import crypto from 'crypto';
 
+interface IUploadConfig {
+  driver: 's3' | 'disk';
+  tmpFolder: string;
+  directory: string;
+  multer: {
+    storage: StorageEngine;
+  };
+  config: {
+    aws: {
+      bucket: string;
+    };
+  };
+}
+
 const uploaderFolder = path.resolve(__dirname, '..', '..', 'uploads');
+const tmpFolder = path.resolve(__dirname, '..', '..', 'temp');
 
 export default {
+  driver: process.env.STORAGE_DRIVER || 'disk',
   directory: uploaderFolder,
-  storage: multer.diskStorage({
-    destination: uploaderFolder,
-    filename(request, file, cb) {
-      const hash = crypto.randomBytes(10).toString('hex');
-      const filename = `${hash}-${file.originalname}`;
+  tmpFolder,
+  multer: {
+    storage: multer.diskStorage({
+      destination: tmpFolder,
+      filename(request, file, cb) {
+        const hash = crypto.randomBytes(10).toString('hex');
+        const filename = `${hash}-${file.originalname}`;
 
-      cb(null, filename);
+        cb(null, filename);
+      },
+    }),
+  },
+
+  config: {
+    aws: {
+      bucket: 'api-vendas',
     },
-  }),
-};
+  },
+} as IUploadConfig;
